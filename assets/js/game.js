e054b4888aabd1ced81b5c8cae29ef1ace769df4
kea6t <keasay26@gmail.com>
2022-06-02 01:46:16 -0400

    learned to create and execute functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// This creates a variable named playerName and sets it equal to the player's input.
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message.");
+// This will do math and add log 20 to the console.
+console.log(10 + 10);
+
+// What is this?
+console.log("Our robot's name is " + playerName);
+
+// This creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
